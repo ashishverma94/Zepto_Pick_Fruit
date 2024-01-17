6f487c75b7c083f4798ddff3b7c59879d4848057
ashishverma94 <hariashish1@gmail.com>
2024-01-17 19:48:34 +0530

    mark last deleted tag

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -3,68 +3,73 @@ import FruitData from '../data/Data'
 import ChipCardOptions from './chips/ChipCardOptions.jsx';
 
 const AutoComplete = ({ options = FruitData }) => {
-    const [markDelete,setMarkDelete] = useState(0) ;
-    const [isPermitted, setIsPermitted] = useState(new Array(options.length).fill(true)) ;
-    let   [mySuggestions, setMySuggestions ] = useState(options)
+    const [markDelete, setMarkDelete] = useState(0);
+    const [isPermitted, setIsPermitted] = useState(new Array(options.length).fill(true));
+    let   [mySuggestions, setMySuggestions] = useState(options)
     const [tags, setTags] = useState([]);
     const [value, setValue] = useState("");
     const [showSuggestions, setShowSuggestions] = useState(false);
-    const refOne = useRef(null) ;
-    const refTwo = useRef() ;
+    const [alength, setAlength] = useState(0) ;
+    const [borderFlag, setBorderFlag] = useState([]) ;
+    const refOne = useRef(null);
+    const refTwo = useRef();
 
     // UPDATE SUGGESTIONS 
-    const updatePermitValues=(value,flag)=>{
-        for ( let i = 0; i < isPermitted.length; i++ ){
-            if ( options[i].name === value){
-                isPermitted[i] = flag ;
+    const updatePermitValues = (value, flag) => {
+        for (let i = 0; i < isPermitted.length; i++) {
+            if (options[i].name === value) {
+                isPermitted[i] = flag;
             }
         }
     }
 
     // HANDLE OUTSIDE CLICK
-    useEffect(()=>{
-        document.addEventListener('click', handleClickOutside,true)
-    },[])
+    useEffect(() => {
+        document.addEventListener('click', handleClickOutside, true)
+    }, [])
 
     // MOVE CURSOR AUTOMATICALLY TO INPUT FIELD
-    useEffect(()=>{
-        if ( refTwo.current.value == '' && tags.length > 0)
-            refTwo.current.focus() ;
-    },[tags])
+    useEffect(() => {
+        if (refTwo.current.value === '' && tags.length > 0)
+            refTwo.current.focus();
+    }, [tags])
 
-    const handleClickOutside = (e)=>{
-        if ( !refOne.current.contains(e.target)){
+    const handleClickOutside = (e) => {
+        if (!refOne.current.contains(e.target)) {
             setShowSuggestions(false)
         }
     }
-    
+
     // HANDLE MAKING TAGS
-    const handleKeyDown = (e,i) => {
-        // if ( markDelete === 0 ){
-        //     setMarkDelete(1) ;
-        //     return ;
-        // }else{
-        //     setMarkDelete(0) ;
-        // }
+    const handleKeyDown = (e, i) => {
+        if ( markDelete === 0 ){
+            setMarkDelete(1) ;
+            borderFlag[borderFlag.length-1] = '3px solid red'
+            return ;
+        }else{
+            setMarkDelete(0) ;
+        }
 
         if (e.target.value === "" && tags.length > 0 && e.key === 'Backspace') {
-            const isDeleted = tags[tags.length-1][0] ;
-            updatePermitValues(isDeleted,true) ;
+            const isDeleted = tags[tags.length - 1][0];
+            updatePermitValues(isDeleted, true);
             tags.pop();
-            console.log("hi")
+            setAlength(alength-1) ;
             setTags([...tags]);
-            isPermitted[i] = true ;
+            isPermitted[i] = true;
+            borderFlag.pop() ;
         }
     }
 
     // REMOVE CHIP WHEN (x) IS PRESSED
-    const removeTag=(index, tag)=>{
-        updatePermitValues(tag,true) ;
-        setTags(tags.filter((el,i) => i!== index))
+    const removeTag = (index, tag) => {
+        updatePermitValues(tag, true);
+        setTags(tags.filter((el, i) => i !== index))
+        borderFlag.pop() ;
     }
 
     // HANDLE AUTOCOMPLETE
-    mySuggestions = options.filter((option,i) => {
+    mySuggestions = options.filter((option, i) => {
         return isPermitted[i] && option.name.toLowerCase().includes(value.toLocaleLowerCase())
     });
 
@@ -74,27 +79,30 @@ const AutoComplete = ({ options = FruitData }) => {
     }
 
     const handleSuggestionClick = (newFruit, newUrl = "", i) => {
-        updatePermitValues(newFruit,false) ;
-        setTags([...tags, [newFruit,newUrl]]);
-        setValue('') ;
+        updatePermitValues(newFruit, false);
+        setTags([...tags, [newFruit, newUrl]]);
+        setAlength(alength+1) ;
+        borderFlag.push('none')
+        setValue('');
     }
 
     return (
-        <div ref = {refOne} className='tags-input-conainer'>
-            {
-                tags.map((tag, index) => (
-                    <div className='tag-item' key={index}>
-                        <img src = {tag[1]} height='20px' width='20px' alg="image-tag"/>
-                        <span className='text'>{tag[0]}</span>
+        <div ref={refOne} className='tags-input-conainer'>
+                {
+                    tags.map((tag, index) => (
+                        <div className='tag-item' style={{border: borderFlag[index]}} key={index}>
+                            <img src={tag[1]} height='20px' width='20px' alt="TagImage" />
+                            <span className='text'>{tag[0]}</span>
 
-                        <span className='close' onClick={() => removeTag(index,tag[0])}><button>&times;</button></span>
-                    </div>
-                ))
-            }
+                            <span className='close' onClick={() => removeTag(index, tag[0])}><button>&times;</button></span>
+                        </div>
+                    ))
+                }
+
 
             <input
-                className="tags-input" 
-                type='text' 
+                className="tags-input"
+                type='text'
                 placeholder='Add new fruit'
                 ref={refTwo}
                 value={value}
@@ -108,7 +116,7 @@ const AutoComplete = ({ options = FruitData }) => {
                     <ul className='suggestions'>
                         {mySuggestions.map((suggestion, id) => (
                             <li
-                                onClick={() => handleSuggestionClick(suggestion.name,suggestion.image, id)}
+                                onClick={() => handleSuggestionClick(suggestion.name, suggestion.image, id)}
                                 key={id}
                             >
                                 <ChipCardOptions
